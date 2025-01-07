cdc7e4e4643fd0941b67aac9fdfd80e2a16a025b
ChalChalCh.... <k.calvin567@gmail.com>
2025-01-07 12:32:56 +0530

    option enums

--- src/main.rs
+++ src/main.rs
@@ -44,6 +44,25 @@ fn main() {
         height:70,
     };
     println!("{}",react.area());
+
+    // option enums
+    let index = find_first_a(String::from("preet"));
+
+    match index{
+        Some(value) => println!("index is {}",value),
+        None => println!("a not found"),
+    }
+
+}
+
+fn find_first_a(s: String) -> Option<i32>{
+    for (index,char) in s.chars().enumerate(){
+        if char == 'a' {
+            return Some(index as i32);
+        }
+    }
+
+    return None;
 }
 
 struct User{
